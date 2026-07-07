477be276e736198bf6c1e3832444d734d4cb35e7
Duke <161149572+DukeOAngels@users.noreply.github.com>
2026-07-07 21:19:02 +0300

    Delete js directory

--- js/theme.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggle = document.getElementById("theme-toggle");
-    const body = document.body;
-
-    toggle.addEventListener("click", () => {
-        body.classList.toggle("dark-mode");
-        body.classList.toggle("light-mode");
-    });
-});
